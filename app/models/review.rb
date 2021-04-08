db812322971f678321170e34d6fa3a3ffd881433
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-04-07 20:42:12 -0400

    valid/assoc. for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,8 @@
 class Review < ApplicationRecord
+  belongs_to :user
+  belongs_to :movie
+
+  validates :comment, presence: true
+  validated :rating, numericality: { in: 0..100 }
+  validates :user_id, :uniqueness: { scope: :movie_id }
 end
