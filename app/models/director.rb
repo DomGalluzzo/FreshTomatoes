846c803c2f85f239808ca43b158ba1e4c203b097
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-04-07 20:35:39 -0400

    valid/assoc. for directors

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,2 +1,7 @@
 class Director < ApplicationRecord
+  has_many :movies, dependent: :destroy
+  has_many :genres, through: :movies
+
+  validates :name, presence: true
+  validates :image, presence: true
 end
