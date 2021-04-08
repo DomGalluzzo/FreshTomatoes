89133fe0f7f970922e644f4f7dd4b8af8bb828a6
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-04-07 20:48:21 -0400

    valid/assoc. for watchlist

--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -1,3 +1,8 @@
 class Watchlist < ApplicationRecord
   belongs_to :user
+
+  has_many :favorites, dependent: :destroy
+  has_many :movies, through: :watchlists
+
+  validates :name, presence: true
 end
