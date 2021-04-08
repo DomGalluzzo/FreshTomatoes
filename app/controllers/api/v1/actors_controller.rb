3217ec0f428826991b39981533f1e1173d335e12
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-04-08 13:10:16 -0400

    index and show methods for api actors controller

--- app/controllers/api/v1/actors_controller.rb
+++ app/controllers/api/v1/actors_controller.rb
@@ -1,6 +1,17 @@
 module Api
   module V1
     class ActorsController < ApplicationController
+      skip_before_action :authenticate_user!, only: %i[index show]
+
+      def index
+        actors = Actor.all
+        render json: actors
+      end
+
+      def show
+        actor = Actor.find(params[:id])
+        render json: actor
+      end
     end
   end
 end
