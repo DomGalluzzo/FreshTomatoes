e4c47b9100b8bc60f1c39b7a768a634e9c9eb241
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-04-07 20:51:23 -0400

    valid/assoc. for credits. typo fix in review

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,4 +1,6 @@
 class Credit < ApplicationRecord
   belongs_to :movie
   belongs_to :actor
+
+  validates :cast, length: { minimum: 1 }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,6 @@ class Review < ApplicationRecord
   belongs_to :movie
 
   validates :comment, presence: true
-  validated :rating, numericality: { in: 0..100 }
+  validates :rating, numericality: { in: 0..100 }
   validates :user_id, uniqueness: { scope: :movie_id }
 end
