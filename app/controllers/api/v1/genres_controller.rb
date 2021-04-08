1a792e65d7f9c4961205564d6c976fa8a43b5d92
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-04-08 14:43:52 -0400

    index show actions for genres

--- app/controllers/api/v1/genres_controller.rb
+++ app/controllers/api/v1/genres_controller.rb
@@ -1,6 +1,17 @@
 module Api
   module V1
     class GenresController < ApplicationController
+      skip_before_action :authenticate_user!, only: %i[index show]
+
+      def index
+        @genres = Genre.all
+        render json: @genres
+      end
+
+      def show
+        @genre = Genre.find(params[:id])
+        render json: @genre
+      end
     end
   end
 end
