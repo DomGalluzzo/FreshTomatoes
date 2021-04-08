f13d7bc827e5c5093f51e756158abfbd18d1557d
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-04-08 14:51:48 -0400

    methods for credits

--- app/controllers/api/v1/credits_controller.rb
+++ app/controllers/api/v1/credits_controller.rb
@@ -1,11 +1,16 @@
 module Api
   module V1
     class CreditsController < ApplicationController
-      skip_before_action :authenticate_user!, only: :index
+      skip_before_action :authenticate_user!, only: %i[index show]
 
       def index
         @credits = Credit.all
         render json: @credits
+      end
+
+      def show
+        @credit = Credit.find(params[:id])
+        @movie = @credit.movie
       end
     end
   end
