701c1596c2c25e04e0a7c2548d640c7922740d22
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-04-12 10:00:30 -0400

    fetch movies action

--- app/javascript/app/actions/index.js
+++ app/javascript/app/actions/index.js
@@ -11,18 +11,30 @@
 // 	console.log(data);
 // };
 
-export const INCREMENT = "INCREMENT";
+// export const INCREMENT = "INCREMENT";
 
-export const DECREMENT = "DECREMENT";
+// export const DECREMENT = "DECREMENT";
 
-export const increment = () => ({
-	type: "INCREMENT",
-});
+// export const increment = () => ({
+// 	type: "INCREMENT",
+// });
 
-export const decrement = () => ({
-	type: "DECREMENT",
-});
+// export const decrement = () => ({
+// 	type: "DECREMENT",
+// });
 
-export const initialState = {
-	count: 0,
+// export const initialState = {
+// 	count: 0,
+// };
+
+export const FETCH_MOVIES = "FETCH MOVIES";
+
+export const fetchMovies = async () => {
+	const response = await fetch("/api/v1/movies");
+	const data = await response.json();
+
+	return {
+		type: FETCH_MOVIES,
+		payload: data,
+	};
 };
