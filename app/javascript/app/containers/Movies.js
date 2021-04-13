35b63fd0a444c018ebdd0c205ad05806636319fe
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-04-13 12:04:09 -0400

    moved Movies.js from components folder to containers folder since it needs access to redux state

---
+++ app/javascript/app/containers/Movies.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import Axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { Col, Row } from "react-bootstrap";
+
+import { fetchMovies } from "../actions/index";
+import Movie from "../components/Movie";
+
+const Movies = () => {
+	const dispatch = useDispatch();
+	const movies = useSelector((state) => state.movies);
+	console.log(movies);
+
+	useEffect(() => {
+		dispatch(fetchMovies());
+	}, []);
+
+	return (
+		<Row>
+			{movies.map((movie) => {
+				return (
+					<Col md={4} key={movie.id}>
+						<Movie movie={movie} />
+					</Col>
+				);
+			})}
+		</Row>
+	);
+};
+
+export default Movies;
