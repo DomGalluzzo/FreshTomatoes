a1b6f1f847e7dafa8cdf55bda1a4c47e27eda308
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-04-17 14:43:09 -0400

    default image check

--- app/javascript/app/components/MovieActorsList.js
+++ app/javascript/app/components/MovieActorsList.js
@@ -6,6 +6,15 @@ import { Card, ListGroup, Image } from "react-bootstrap";
 import Title from "../components/Title";
 
 const MovieActorsList = ({ actors }) => {
+	// Determine the avatarImage path depending on the existance of a valid
+	// user id. If user id is invalid, fallback to an image based on gender
+	// const avatarImage =
+	// 	id !== undefined && id !== null
+	// 		? `/users/${id}/avatar.jpg`
+	// 		: `/${gender}.jpg`;
+
+	const defaultProfilePic = "/images/DefaultIcon.png";
+
 	return (
 		<Card>
 			<ListGroup variant="flush">
@@ -16,12 +25,12 @@ const MovieActorsList = ({ actors }) => {
 							to={`/actors/${actor.id}`}
 							style={{ textDecoration: "inherit", color: "inherit" }}>
 							<ListGroup.Item>
-								<Image
+								<img
 									id="movie-show-poster"
-									src={actor.image}
+									src={actor.image ? actor.image : defaultProfilePic}
 									alt={`${actor.name}.jpg`}
 								/>
-								<span className="pl-3">{actor.name}</span>
+								<span className="ml-3">{actor.name}</span>
 							</ListGroup.Item>
 						</Link>
 					);
