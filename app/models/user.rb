8de476b320b4634ec9feadd75254635ea2663376
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-04-20 10:12:16 -0400

    model user association for attached photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,7 @@ class User < ApplicationRecord
   # has_many :favorites, dependent: :destroy
   has_many :movies, through: :watchlists
 
+  has_one_attached :photo
+
   validates :username, uniqueness: true
 end
