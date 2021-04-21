71edb71672daf499e64a7d235e74eef6ea26b480
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-04-21 11:02:40 -0400

    added images column to users table in db

--- app/javascript/app/index.js
+++ app/javascript/app/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { Container } from "react-bootstrap";
+import { CloudinaryContext } from "cloudinary-react";
 
 import store from "./components/store";
 import App from "./components/App";
@@ -10,10 +11,12 @@ import TopNav from "./components/TopNav";
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<TopNav />
-			<Container className="main-container p-0">
-				<App />
-			</Container>
+			<CloudinaryContext cloudName="dcdspz5mv">
+				<TopNav />
+				<Container className="main-container p-0">
+					<App />
+				</Container>
+			</CloudinaryContext>
 		</Provider>
 	</React.StrictMode>,
 	document.getElementById("root")
