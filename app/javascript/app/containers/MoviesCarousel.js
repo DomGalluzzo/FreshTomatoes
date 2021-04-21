d8fddbf206e6dd22fa5e04ba80b4132761478448
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-04-21 16:13:32 -0400

    added 2 pictures to side of carousel

--- app/javascript/app/containers/MoviesCarousel.js
+++ app/javascript/app/containers/MoviesCarousel.js
@@ -1,10 +1,18 @@
 import React from "react";
-
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import _ from "lodash";
 
-import { Col, Row, Container, Carousel } from "react-bootstrap";
+import { Col, Row, Container, Carousel, Image } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faFacebookSquare,
+	faTwitter,
+	faInstagram,
+	faSnapchatGhost,
+	faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+// import {  } from "@fortawesome/free-solid-svg-icons";
 
 import { fetchMoviesList } from "../actions/index";
 import Title from "../components/Title";
@@ -62,16 +70,56 @@ const MoviesCarousel = () => {
 
 	return (
 		<Container className="movies-carousel-container px-0">
-			<Col md={6} className="px-0">
-				<div className="carousel-title-div">
-					<span className="px-2">Trending on FT:</span>
+			<div className="carousel-title-div">
+				<span className="px-2">Trending on FT:</span>
+				<div className="carousel-title">
 					<Title
 						text="Critics are STILL raving about these top classics"
 						className="carousel-title"
 					/>
 				</div>
-				<Carousel>{showData()}</Carousel>
-			</Col>
+			</div>
+			<Row noGutters={true}>
+				<Col md={6} className="px-0">
+					<Carousel>{showData()}</Carousel>
+				</Col>
+				<Col md={3} className="px-0">
+					<a
+						href="https://www.oscars.org/"
+						target="_blank"
+						style={{ textDecoration: "none" }}>
+						<Image
+							src="https://res.cloudinary.com/dcdspz5mv/image/upload/v1619033134/oscars_mrit9x.jpg"
+							className="home-image"
+							style={{ height: "259px", width: "285px" }}
+						/>
+						<div className="image-overlay-text">
+							<p className="image-overlay-header px-3 py-2">
+								<span>Watch the Oscars!</span> <br />
+								Tune in April 25th
+							</p>
+						</div>
+					</a>
+				</Col>
+				<Col md={3} className="px-0">
+					<Link to="/movies" style={{ textDecoration: "none" }}>
+						<Image
+							src="https://res.cloudinary.com/dcdspz5mv/image/upload/v1619033134/vote_mve6ah.jpg"
+							className="home-image"
+							style={{
+								height: "259px",
+								width: "286px",
+							}}
+						/>
+						<div className="image-overlay-text">
+							<p className="image-overlay-header px-3 py-2">
+								<span>Vote for your favorite movies!</span> <br />
+								Ballots are due at the end of June
+							</p>
+						</div>
+					</Link>
+				</Col>
+			</Row>
 		</Container>
 	);
 };
