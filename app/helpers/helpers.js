5261315115a395285dbed57ba84e4ff3df781225
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-04-24 10:11:49 -0400

    updated helper functions to arrow functions. pages_controller conditional for logged in user

--- app/helpers/helpers.js
+++ app/helpers/helpers.js
@@ -1,15 +1,15 @@
 // ! Converts runtime minutes into xhour and xminutes format
-export function runtimeConversion(n) {
+export const runtimeConversion = (n) => {
 	let num = n;
 	let hours = num / 60;
 	let rhours = Math.floor(hours);
 	let minutes = (hours - rhours) * 60;
 	let rminutes = Math.round(minutes);
 	return `${rhours}h ${rminutes}m`;
-}
+};
 
 // ! Compute a movie's average rating score
-export function averageRating(reviews) {
+export const averageRating = (reviews) => {
 	let xReviews = reviews;
 	let total = 0;
 
@@ -23,4 +23,4 @@ export function averageRating(reviews) {
 
 	total = Math.floor((total / 50) * 100);
 	return total;
-}
+};
